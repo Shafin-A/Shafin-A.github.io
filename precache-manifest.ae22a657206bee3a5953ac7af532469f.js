30953df2be5a208d48f1056aae43f0c39a98080c
Shafin <kazishafin@gmail.com>
2020-06-08 21:14:53 -0400

    Updates

---
+++ precache-manifest.ae22a657206bee3a5953ac7af532469f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a742e8925509bcf669848f30017bd4a4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70a175771aa1929e6401",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2df1a7e1fb3d862f2ce3",
+    "url": "/portfolio/static/js/2.4a17821a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.4a17821a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a175771aa1929e6401",
+    "url": "/portfolio/static/js/main.7b1dd2fe.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "9fcbc30979c0164a400380630163f060",
+    "url": "/portfolio/static/media/ShafinAhmedCV.9fcbc309.pdf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b9c8501c8629bf4aeca8b776d06f052d",
+    "url": "/portfolio/static/media/myAvatar.b9c8501c.png"
+  }
+]);
