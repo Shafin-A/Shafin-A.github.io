7e97c5f747f02b0596b6c8ecbeb434587ae81bd9
Shafin <kazishafin@gmail.com>
2020-06-09 12:44:58 -0400

    react pdf test

--- portfolio/src/Components/Resume.js
+++ portfolio/src/Components/Resume.js
@@ -1,49 +1,17 @@
 import React from 'react';
 import resumePDF from '../graphics/ShafinAhmedCV.pdf'
-import { withStyles } from '@material-ui/styles';
-import PropTypes from 'prop-types';
+import { Document, Page, pdfjs } from 'react-pdf/dist/entry.webpack';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-const styles = theme => ({
-	container: {
-		position: 'relative',
-		overflow: 'hidden',
-		paddingTop: '56.25%'
-	},
-	resume: {
-		position: 'absolute',
-		top: 0,
-		left: 0,
-		bottom: 0,
-		right: 0,
-		width: '100%',
-		height: '100%'
-	}
-});
-
-
-export class Resume extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-  	const { classes } = this.props; 
+export default function Resume() {
+  	pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
     return (
-      <div className={classes.container}>
-        <iframe
-        	className={classes.resume}
-            src={resumePDF}
-        ></iframe>
-      </div>
+    	<div>
+			<Document file={resumePDF}>
+				<Page pageNumber={1}/>
+			</Document>
+		</div>
     );
   }
-}
 
-
-Resume.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Resume);
